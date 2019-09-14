d89b97c6746e845cb54c2eeb00b88f69d5dfa077
ndemo <ndemorich@gmail.com>
2019-09-14 23:30:31 +0300

    First commit

---
+++ roadinspectiontool/do_login.php
@@ -0,0 +1,31 @@
+<?php session_start();
+include 'database/config.php';
+
+// declare post fields
+if(isset($_POST['submit'])){
+$post_username = trim($_POST['username']);
+$post_password = trim($_POST['password']);
+
+//$post_autologin = $_POST['autologin'];
+
+$result = mysqli_query($link,"SELECT * FROM tbuser WHERE (username = '" .addslashes($post_username) . "') and (password = '" .$post_password. "')");
+
+if (!$result) {
+    printf("Error: %s\n", mysqli_error($link));
+    exit();
+}
+if($row=mysqli_fetch_array($result)){
+
+$_SESSION['username'] = $row['fname'];
+$_SESSION['priv'] = $row['priv'];
+// Autologin Requested?
+
+echo "OK";
+}
+else
+{
+echo '<div id="error_notification">Invalid username and password.</div>';
+}
+
+}
+?>
